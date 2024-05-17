e16dccaf058f6076be3b3c8bea2965c19da3ec75
Ella Vasu shiva nageswarrao <122893770+Vasuella@users.noreply.github.com>
2024-05-17 16:01:54 +0530

    Initial commit

---
+++ Jobportal/Jobportal/urls.py
@@ -0,0 +1,35 @@
+"""
+URL configuration for Jobportal project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from job.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index, name='index'),
+path('Admin_login', Admin_login, name='Admin_login'),
+path('user_login', user_login, name='user_login'),
+path('recruiter_login', recruiter_login, name='recruiter_login'),
+path('user_signup', user_signup, name='user_signup'),
+path('user_home', user_home, name='user_home'),
+path('Logout', Logout, name='Logout'),
+path('recruiter_signup', recruiter_signup, name='recruiter_signup'),
+path('admin_home', admin_home, name='admin_home'),
+path('view_users', view_users, name='view_users'),
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
